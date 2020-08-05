38f4fdee1f25259a250ce9d17648f0df8a5e5896
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-08-05 22:01:48 +0900

    200805 1th

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -6,33 +6,31 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <div className="header">
-                    <Router>
-                        <Link to="/">
-                            <img src={logo} className="logo" />
-                        </Link>
-                    </Router>
+                <Router>
+                    <Link to="/">
+                        <img src={logo} className="logo" />
+                    </Link>
+                </Router>
 
-                    <ul className="login">
-                        <li>
-                            <input type="button" value="SIGN IN" className="h-btn" />
-                        </li>
-                        <li>
-                            <input type="button" value="SIGN UP" className="h-btn" />
-                        </li>
-                    </ul>
-                    <ul className="main">
-                        <li>
-                            <input type="button" value="회사소개" className="h-btn" />
-                        </li>
-                        <li>
-                            <input type="button" value="의뢰하기" className="h-btn" />
-                        </li>
-                        <li>
-                            <input type="button" value="문의하기" className="h-btn" />
-                        </li>
-                    </ul>
-                </div>
+                <ul className="login">
+                    <li>
+                        <input type="button" value="SIGN IN" className="h-btn" />
+                    </li>
+                    <li>
+                        <input type="button" value="SIGN UP" className="h-btn" />
+                    </li>
+                </ul>
+                <ul className="main">
+                    <li>
+                        <input type="button" value="회사소개" className="h-btn" />
+                    </li>
+                    <li>
+                        <input type="button" value="의뢰하기" className="h-btn" />
+                    </li>
+                    <li>
+                        <input type="button" value="문의하기" className="h-btn" />
+                    </li>
+                </ul>
             </header>
         )
     }
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { faChevronRight, faChevronLeft, faCaretDown, faChevronCircleUp } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Slide from 'react-reveal/Slide';
 
 class Main extends React.Component {
     render() {
@@ -13,8 +14,12 @@ class Main extends React.Component {
                     </div>
                     <div className="h_icon">
                         <div className="h_icon-arrow">
-                            <FontAwesomeIcon icon={faChevronLeft} className="arrow-left" />
-                            <FontAwesomeIcon icon={faChevronRight} className="arrow-right" />
+                            <button className="arrow-left">
+                                <FontAwesomeIcon icon={faChevronLeft} />
+                            </button>
+                            <button className="arrow-right">
+                                <FontAwesomeIcon icon={faChevronRight} />
+                            </button>
 
                             <FontAwesomeIcon icon={faCaretDown} className="caret" />
                         </div>
@@ -24,60 +29,77 @@ class Main extends React.Component {
                     </div>
                 </div>
                 <div className="middle_main">
-                    <div className="middle_main-txt1">
-                        <div className="middle_main-txt1-1">
-                            4Leaf SofeWare는
+                    <Slide bottom>
+                        <div className="middle_main-txt1">
+                            <div className="middle_main-txt1-1">
+                                4Leaf SofeWare는
                         </div>
-                        <div className="middle_main-txt1-2">
-                            Web과 App개발을 의뢰 받아 만드는 회사입니다.
+                            <div className="middle_main-txt1-2">
+                                Web과 App개발을 의뢰 받아 만드는 회사입니다.
                         </div>
-                    </div>
-                    <div className="middle_main-txt2">
-                        <div className="middle_main-txt2-1">
-                            4Leaf SofeWare는 의뢰접수 → 시안제작 →  개발시작
+                        </div>
+                    </Slide >
+                    <Slide bottom>
+
+                        <div className="middle_main-txt2">
+                            <div className="middle_main-txt2-1">
+                                4Leaf SofeWare는 의뢰접수 → 시안제작 →  개발시작
                         </div>
 
-                        <div className="middle_main-txt2-2">
-                            으로 진행됩니다
+                            <div className="middle_main-txt2-2">
+                                으로 진행됩니다
                         </div>
-                    </div>
+                        </div>
+                    </Slide >
                     <div className="btn">
                         <div>
-                            <input type="button" value="포트폴리오 →" />
+                            <Slide left>
+
+                                <input type="button" value="포트폴리오 →" />
+                            </Slide >
                         </div>
                         <div>
-                            <input type="button" value="개발의뢰 →" />
+                            <Slide right>
+
+                                <input type="button" value="개발의뢰 →" />
+                            </Slide >
                         </div>
                     </div>
                 </div>
                 <div className="bottom_main">
-                    <div className="b-title">
-                        4Leaf SofeWare가 지금까지 만든 Web, App
-                    </div>
-                    <div className="line"></div>
-                    <div className="list">
-                        <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                        <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                        <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                    <Slide bottom>
 
-                        </ul>
+                        <div className="b-title">
+                            4Leaf SofeWare가 지금까지 만든 Web, App
+                        </div>
+                        <div className="line"></div>
+                    </Slide >
+                    <div className="list">
+                        <Slide bottom cascade>
+
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+
+                            </ul>
+                        </Slide>
                     </div>
                 </div>
             </main >
